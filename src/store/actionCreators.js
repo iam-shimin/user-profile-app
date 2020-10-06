df7c21eaf4fbcf85bd004ce01a96aa9855fd06ef
shimin mathew <simplyshimin@gmail.com>
2020-10-07 02:59:25 +0530

    add action creator to fetch profile

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,7 +1,5 @@
 import http, { setToken } from 'utils/http';
 import { loginTypes, registerType } from './actionTypes';
-import { updateToken, clearToken } from 'utils/storage';
-// import history from 'utils/history';
 
 
 export function login(userCredentials, callback) {
@@ -14,6 +12,7 @@ export function login(userCredentials, callback) {
 					type: loginTypes.LOGIN_SUCCESS,
 					payload: response.data.data
 				});
+				setToken(response.data.data.token);
 				callback();
 			})
 			.catch(error => dispatch({
@@ -30,7 +29,6 @@ export function register(userData, callback) {
 			.post('/auth/register', userData)
 			.then(response => {
 				dispatch({ type: registerType.REGISTER_SUCCESS, payload: response.data });
-				updateToken(response.data.data.token);
 				setToken(response.data.data.token);
 				callback();
 			}).catch(error => dispatch({
@@ -40,19 +38,15 @@ export function register(userData, callback) {
 	}
 }
 
-export function logout() {
-	return dispatch => {
-		dispatch({type: 'LOGOUT'});
-		clearToken();
-		document.location = '/';
-	}
-}
-
 export function getProfile() {
 	return dispatch => {
 		dispatch({type: 'PROFILE_FETCH_PENDING'});
-		http.get('/user', {
-			headers: 'session'
-		})
+		http
+			.get('/user')
+			.then(response => {
+				console.log(response)
+				dispatch({type: 'PROFILE_FETCH_SUCCESS', payload: response.data.data})
+			})
+
 	}
 }
