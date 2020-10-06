efce6c20019e10c97158c090270ef1b2e1ff801c
shimin mathew <simplyshimin@gmail.com>
2020-10-06 23:09:07 +0530

    email validation

---
+++ src/components/email.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+import Fieldset from './fieldset';
+
+import emailValidationService from 'services/validateEmail';
+
+export default function Email({ name = "email", onChange }) {
+	const [value, setValue] = useState('');
+	const [error, setError] = useState(null);
+
+	const isValid = !error;
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (value) {
+				setError(null);
+				emailValidationService(value)
+					.then(data => {
+						const emailExist = !data.success;
+						if (emailExist) {
+							setError(data);
+						} else {
+							onChange({target: { name, value }})
+						}
+					})
+					.catch(error => setError(error));
+			}
+		}, 950);
+
+		return () => clearTimeout(timer);
+	}, [value]);
+
+	return (
+	<>
+		<Fieldset
+			label="Email"
+			value={value}
+			name={name}
+			type="email"
+			className={isValid? 'green-br': ''}
+			onChange={event => {
+				const {value} = event.target;
+				setValue(value);
+			}} />
+
+		{error && <small className="validation-error">{error?.message || error?.details || 'Cannot complete request'}</small>}
+	</>
+	)
+}
