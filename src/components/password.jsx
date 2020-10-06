9e63297c4812d3fc74bb31d420903995ada15286
shimin mathew <simplyshimin@gmail.com>
2020-10-06 21:22:14 +0530

    add password component

---
+++ src/components/password.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+import Fieldset from 'components/fieldset';
+
+export default function Password({onChange}) {
+
+	const [value, setValue] = useState({
+		password: '',
+		'c-password': ''
+	});
+
+	const isValid = value.password === value["c-password"];
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setValue(current => ({...current, [name]: value}));
+	}
+
+	useEffect(() => {
+		if (isValid) {
+			onChange({
+				value: value.password,
+				name: 'password'
+			});
+		}
+	}, [isValid]);
+
+	return (
+		<>
+			<Fieldset label="Password" value={value.password} onChange={handleChange} name="password" type="password" />
+			<Fieldset label="Confirm Password" value={value['c-password']} onChange={handleChange} name="c-password" type="password" />
+
+			{hasError && <small className="validation-error">Passwords should match</small>}
+		</>
+	)
+}
