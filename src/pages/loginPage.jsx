0d824968587e84fa2efeb32c3cde015d483c5695
shimin mathew <simplyshimin@gmail.com>
2020-10-06 19:43:02 +0530

    add state to login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LoginPage() {
+
+	const [values, setValues] = useState({
+		email: '',
+		password: ''
+	})
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		// make api call with values
+	}
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setValues(current => ({...current, [name]: value}));
+	}
+
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="email" />
-				<input type="email" id="email"/>
+				<input
+					type="email"
+					value={values.email}
+					onChange={handleChange}
+					id="email"
+					name="email" />
 
 				<label htmlFor="pwd" />
-				<input type="password" id="pwd" />
+				<input
+					type="password"
+					value={values.password}
+					onChange={handleChange}
+					id="pwd"
+					name="password" />
 
 				<button>Submit</button>
 			</form>
