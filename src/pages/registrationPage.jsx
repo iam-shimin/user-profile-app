24d2adc00cedb3a1adc7b98d4fadc8ce0e7fa65f
shimin mathew <simplyshimin@gmail.com>
2020-10-06 20:59:34 +0530

    add basic layout for registration form

--- src/pages/registrationPage.jsx
+++ src/pages/registrationPage.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Fieldset from 'components/fieldset';
 
+import 'style/form.css';
+
 export default function RegistrationPage() {
 	return (
-		<form className="form form--registration-form">
-			<Fieldset label="Email" name="username" />
-			<Fieldset label="Phone" name="phone" type="tel" />
-			<Fieldset label="Password" name="password" type="password" />
-			<Fieldset label="Confirm Password" name="c-password" type="password" />
-			<button>Submit</button>
-		</form>
+		<section>
+			<form className="form form--registration-form">
+				<Fieldset label="Email" name="username" />
+				<Fieldset label="Phone" name="phone" type="tel" />
+				<Fieldset label="Password" name="password" type="password" />
+				<Fieldset label="Confirm Password" name="c-password" type="password" />
+				<button>Submit</button>
+			</form>
+
+			<div className="w-50 mg-a">
+				Have an account? <Link href="/">Log In</Link>
+			</div>
+		</section>
 	)
 }
