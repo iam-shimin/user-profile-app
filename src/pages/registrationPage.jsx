3cedce4ee09e0eab8a03a4ac25b842a057203ccf
shimin mathew <simplyshimin@gmail.com>
2020-10-06 23:14:20 +0530

    add state to registration form

--- src/pages/registrationPage.jsx
+++ src/pages/registrationPage.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Fieldset from 'components/fieldset';
 import Password from 'components/password';
+import Email from 'components/email';
 
 import 'style/form.css';
 
 export default function RegistrationPage() {
+	const [values, setValues] = useState({
+		username: '',
+		email: '',
+		phone: '',
+		password: ''
+	})
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setValues(current => ({...current, [name]: value}));
+	}
+
 	return (
 		<section>
 			<form className="form form--registration-form">
-				<Fieldset label="Email" name="username" />
-				<Fieldset label="Phone" name="phone" type="tel" />
-				<Password onChange={console.log} />
+				<Fieldset label="User Name" value={values.username} name="username" onChange={handleChange} />
+				<Email name="email" value={values.email} onChange={handleChange} />
+				<Fieldset label="Phone" value={values.phone} name="phone" type="tel" onChange={handleChange} />
+				<Password value={values.password} onChange={handleChange} />
 				<button>Submit</button>
 			</form>
 
