07f2c08bf83890f97a2c4958dcadba47c9686792
shimin mathew <simplyshimin@gmail.com>
2020-10-07 03:00:58 +0530

    handle pending and error states in rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,13 +1,41 @@
-import {registerType, loginTypes} from './actionTypes';
+import { registerType, loginTypes, profileFetchTypes } from './actionTypes';
 
 const initialState = {};
 
 export default function rootReducer(state = initialState, action) {
+	if (action.type.endsWith('PENDING')) {
+		return {...state, appPendingState: true};
+	}
+
+	if (action.type.endsWith('FAILURE')) {
+		return {...state, appErrorState: action.payload}
+	}
+
 	switch (action.type) {
 		case registerType.REGISTER_SUCCESS:
-			return {...state, ...action.payload.data.userData};
+			return {
+				...state,
+				...action.payload.data.userData,
+				appPendingState: false,
+				appErrorState: null
+			};
+
 		case loginTypes.LOGIN_SUCCESS:
-			return {...state, ...action.payload.userData}
+			return {
+				...state,
+				...action.payload.userData,
+				appPendingState: false,
+				appErrorState: null
+			};
+
+		case profileFetchTypes.PROFILE_FETCH_SUCCESS:
+			return {
+				...state,
+				...action.payload.userData,
+				appPendingState: false,
+				appErrorState: null
+			};
+			
 		default:
 			return state;
 	}
