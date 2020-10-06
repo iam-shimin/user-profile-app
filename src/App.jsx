bc3546c80af66675e72a7ead9e35b3ccfe403a63
shimin mathew <simplyshimin@gmail.com>
2020-10-06 21:47:18 +0530

    add routes to app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import LoginPage from 'pages/loginPage';
+import WelcomePage from 'pages/welcomePage';
+import RegistrationPage from 'pages/registrationPage';
+import withAuth from 'components/withAuth';
+
+import 'style/App.css';
+
+const AuthedWelcomePage = withAuth(WelcomePage);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegistrationPage} />
+        <Route path="/" exact component={AuthedWelcomePage} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
